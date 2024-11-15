9dd4b22965cb6cb634f8dceeb11e74b780552bbc
Rochibear <killpmite@gmail.com>
2024-11-15 16:14:09 +0800

    Initial Commit

---
+++ secretRPG/secretRPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  secretRPGApp.swift
+//  secretRPG
+//
+//  Created by CihYang Su on 2024/11/15.
+//
+
+import SwiftUI
+
+@main
+struct secretRPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
